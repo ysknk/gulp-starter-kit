409bafde0524c3992d0fa6f9310f5a3bcf71e317
ysknk <simplylife0405@gmail.com>
2018-06-06 09:12:50 +0900

    Update temporary files

--- gulpfile.babel.js/task/config.js
+++ gulpfile.babel.js/task/config.js
@@ -191,7 +191,7 @@ module.exports = {
       module: {
         rules: [
           {
-            test: /\.(js|jsx)$/,
+            test: /\.(js|jsx|json|vue)$/,
             exclude: /node_modules/,
             use: {
               loader: "babel-loader?cacheDirectory=true",
