8215d6c0eb057db0a700a035f2b77cf58baf4fa5
ysknk <31568245+ysknk@users.noreply.github.com>
2019-09-14 21:42:32 +0900

    Update index.js

--- _src/htdocs/assets/js/pages/index.js
+++ _src/htdocs/assets/js/pages/index.js
@@ -1,7 +1,19 @@
 ((win, doc) => {
   'use strict';
 
+  const FN = win[NS];
+  const PAGE_NAME = `${PREFIX}index-page`;
+
+  function isCurrentPage() {
+    let bodyElem = doc.body;
+    return bodyElem.classList.contains(PAGE_NAME);
+  }
+
+  function init() {}
+
   doc.addEventListener('DOMContentLoaded', (e) => {
+    if (!isCurrentPage()) return;
+    init();
   }, false);
 
 })(window, document);
