b614b4a0474f5dd57bdd6001e7483c9187b4e668
ysknk <31568245+ysknk@users.noreply.github.com>
2019-09-26 14:16:56 +0900

    Update postinstall.js

--- postinstall.js
+++ postinstall.js
@@ -14,12 +14,9 @@ const dir = {
 };
 
 const message = {
-  postInstallComplete: `\npostinstall complete!\n`,
-  copyComplete: `file copy complete!\n`,
-  notCopy: `file exists. not copy.\n`,
-  error: {
-    nodejs: `\nplease install nodejs or upgrade nodejs version.`
-  }
+  postInstallComplete: `postinstall complete!\n`,
+  copyComplete: `file copy complete!`,
+  notCopy: `file exists. not copy.`
 };
 
 (function() {
@@ -27,38 +24,51 @@ const message = {
   let colors = {
     red: (str) => {return `\u001b[31m${str}\u001b[0m`;},
     magenta: (str) => {return `\u001b[35m${str}\u001b[0m`;},
-    cyan: (str) => {return `\u001b[36m${str}\u001b[0m`;}
+    cyan: (str) => {return `\u001b[36m${str}\u001b[0m`;},
+    blue: (str) => {return `\u001b[34m${str}\u001b[0m`;},
+    lightBlue: (str) => {return `\u001b[96m${str}\u001b[0m`;},
+    lightGreen: (str) => {return `\u001b[92m${str}\u001b[0m`;}
   };
+  let theme_color = `lightBlue`;
   let fs = ``;
+  let isInitialize = false;
 
   new Promise((resolve, reject) => {
     fs = require('fs-extra');
 
-    title(`base files copy destination directory [${dir.src} => ${dir.dest}]`);
+    title(`base files copy destination directory begin. [${dir.src} => ${dir.dest}]`);
 
     if (checkFile(fs, dir.dest)) {
       console.log(message.notCopy)
+      title(``, true);
       return resolve();
     }
 
-    return fsCopy({
+    isInitialize = true;
+    fsCopy({
       clobber: true
+    }, () => {
+      title(``, true);
     }, resolve, reject);
 
   }).then(() => {
-    title(`${cmd.install} begin. [${dir.config}]`);
+    title(`${cmd.install} [${dir.config}]`);
     execSync(`cd ${dir.config} && ${cmd.install}`, {stdio:[0,1,2]});
-    title(`${cmd.install} end. [${dir.config}]`);
-  }).then(() => {
-    title(`${cmd.build} begin.`);
-    execSync(cmd.build, {stdio:[0,1,2]});
-    title(`${cmd.build} end.`);
+    title(``, true);
 
   }).then(() => {
-    console.log(message.postInstallComplete)
+    if (isInitialize) {
+      title(`${cmd.build}`);
+      execSync(cmd.build, {stdio:[0,1,2]});
+      title(``, true);
+    }
+
+  }).then(() => {
+    console.log(colors.lightGreen(message.postInstallComplete));
 
   }).catch((e) => {
     console.log(e);
+
   });
 
   function checkFile(fs, filepath) {
@@ -73,21 +83,25 @@ const message = {
     }
   }
 
-  function title(message, mark = '-', repeat = 30) {
-    return console.log([
-      colors.magenta(`/*${mark.repeat(repeat)}`),
-      colors.magenta('  ' + message),
-      colors.magenta(`${mark.repeat(repeat)}*/`),
-    ].join('\n'));
+  function title(message, isEnd = false, mark = '-', repeat = 30) {
+    return console.log(isEnd
+      ? [colors[theme_color](`/*${mark.repeat(repeat - 2)}*/\n`)].join('\n')
+      : [
+        colors[theme_color](`/*${mark.repeat(repeat)}`),
+        colors[theme_color]('  ' + message),
+        colors[theme_color](`${mark.repeat(repeat)}*/${isEnd ? '\n' : ''}`),
+      ].join('\n'));
   }
 
-  function fsCopy(opt, resolve, reject) {
+  function fsCopy(opt, callback, resolve, reject) {
     fs.copy(dir.src, dir.dest, opt, (err) => {
       if (err) {
         console.error(err);
+        callback && callback();
         reject();
       }
       console.log(message.copyComplete);
+      callback && callback();
       resolve();
     });
   }
