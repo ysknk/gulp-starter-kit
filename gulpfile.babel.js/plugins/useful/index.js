3177748430aa6ef83c21aeb3ac1388727b8d2585
ysknk <31568245+ysknk@users.noreply.github.com>
2019-10-08 14:38:50 +0900

    Update index.js

--- gulpfile.babel.js/plugins/useful/index.js
+++ gulpfile.babel.js/plugins/useful/index.js
@@ -26,11 +26,11 @@ module.exports = (opts_) => {
     }
   }, opts_);
 
-  if(!opts_.encode.from) {
+  if (!opts_.encode.from) {
     opts_.encode.from = defaultEncode;
   }
 
-  if(!opts_.encode.iconv) {
+  if (!opts_.encode.iconv) {
     opts_.encode.iconv = iconv ?
       iconv : {decode: {}, encode: {}};
   }
@@ -47,15 +47,15 @@ module.exports = (opts_) => {
    *          error argument and data) when you are done processing the supplied chunk.
    */
   transformStream._transform = (file, encoding, callback) => {
-    if(file.isNull()) {
+    if (file.isNull()) {
       return callback(null, file);
     }
 
-    if(file.isStream()) {
+    if (file.isStream()) {
       return callback(new pluginError(pluginName, text.stream));
     }
 
-    if(file.isBuffer()) {
+    if (file.isBuffer()) {
       let encodeFrom = opts_.encode.from;
       let encodeTo = opts_.encode.to;
 
@@ -68,17 +68,17 @@ module.exports = (opts_) => {
       let matchList = [];
 
       // trim head line
-      if(opts_.trimheadline) {
+      if (opts_.trimheadline) {
         contents = contents.replace(/^(\r\n|\n\r|\n|\r)/, '');
       }
 
-      if(replace && replace.length || find && find.length) {
+      if (replace && replace.length || find && find.length) {
         // replace
-        if(replace.length) {
+        if (replace.length) {
           _.forEach(replace, (content) => {
             let ex = new RegExp('(' + content.from + ')', 'g');
             let isMatch = contents.match(ex);
-            if(isMatch) {
+            if (isMatch) {
               contents = contents.replace(ex, content.to);
               matchList.push([
                 colors.yellow([
@@ -93,11 +93,11 @@ module.exports = (opts_) => {
         }
 
         // find
-        if(find.length) {
+        if (find.length) {
           _.forEach(find, (content) => {
             let ex = new RegExp('(' + content + ')', 'g');
             let isMatch = contents.match(ex);
-            if(isMatch) {
+            if (isMatch) {
               matchList.push([
                 colors.yellow([
                   '  find*' + isMatch.length,
@@ -109,7 +109,7 @@ module.exports = (opts_) => {
         }
 
         // show match logs
-        if(matchList.length) {
+        if (matchList.length) {
           fancyLog('- ' + colors.blue(filename));
           _.forEach(matchList, (list) => {
             fancyLog(list);
@@ -121,13 +121,13 @@ module.exports = (opts_) => {
       let code = new RegExp('(\n|\r\n|\r)', 'g');
       let change = '';
 
-      if(opts_.linefeedcode.match(/^CRLF$/i)) {
+      if (opts_.linefeedcode.match(/^CRLF$/i)) {
         change = '\r\n';
-      }else if(opts_.linefeedcode.match(/^CR$/i)) {
+      } else if(opts_.linefeedcode.match(/^CR$/i)) {
         change = '\r';
-      }else if(opts_.linefeedcode.match(/^LF$/i)) {
+      } else if(opts_.linefeedcode.match(/^LF$/i)) {
         change = '\n';
-      }else{
+      } else {
         // default
         change = '\n';
       }
@@ -135,7 +135,7 @@ module.exports = (opts_) => {
       file.contents = new Buffer.from(contents);
 
       // encode
-      if(encodeTo != defaultEncode ||
+      if (encodeTo != defaultEncode ||
         encodeFrom != defaultEncode) {
         let content = iconv.decode(file.contents, encodeFrom, opts_.encode.iconv.decode);
         file.contents = iconv.encode(content, encodeTo, opts_.encode.iconv.encode);
