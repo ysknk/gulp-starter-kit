9f39385f9f1a3dae5a497a33cffe013bea372a05
ysknk <ysknk1211@gmail.com>
2019-10-13 23:17:22 +0900

    Update postinstall.js

--- postinstall.js
+++ postinstall.js
@@ -3,7 +3,6 @@
 
   const execSync = require('child_process').execSync;
 
-  let inputMark = `> `;
   let colors = {
     red: (str) => {return `\u001b[31m${str}\u001b[0m`;},
     magenta: (str) => {return `\u001b[35m${str}\u001b[0m`;},
@@ -38,90 +37,44 @@
   let fs = ``;
   let isInitialize = false;
 
+  // _src
   new Promise((resolve, reject) => {
     fs = require('fs-extra');
-
     title(`base files copy. [${dir.src} => ${dir.dest}]`);
+    copyFiles(dir.src, dir.dest, () => {
+      isInitialize = true;
 
-    if (checkFile(fs, dir.dest)) {
-      console.log(message.notCopy)
-      title(``, true);
-      return resolve();
-    }
-
-    isInitialize = true;
-
-    // _src
-    fsCopy(dir.src, dir.dest, {
-      clobber: true
-    }, () => {
-      title(``, true);
-    }, resolve, reject);
-
+      resolve();
+    }, resolve);
+  // gitignore
   }).then(() => {
     return new Promise((resolve, reject) => {
       title(`files copy. [${dir.gitignore} => ${dir.root}]`);
-
-      if (checkFile(fs, `${dir.root}${dir.gitignore}`)) {
-        console.log(message.notCopy)
-        title(``, true);
-        return resolve();
-      }
-
-      // gitignore
-      fsCopy(dir.gitignore, `${dir.root}${dir.gitignore}`, {}, () => {
-        title(``, true);
-      }, resolve, reject);
+      copyFiles(dir.gitignore, `${dir.root}${dir.gitignore}`, resolve, resolve);
     });
-
+  // editorconfig
   }).then(() => {
     return new Promise((resolve, reject) => {
       title(`files copy. [${dir.editorconfig} => ${dir.root}]`);
-
-      if (checkFile(fs, `${dir.root}${dir.editorconfig}`)) {
-        console.log(message.notCopy)
-        title(``, true);
-        return resolve();
-      }
-
-      // editorconfig
-      fsCopy(dir.editorconfig, `${dir.root}${dir.editorconfig}`, {}, () => {
-        title(``, true);
-      }, resolve, reject);
+      copyFiles(dir.editorconfig, `${dir.root}${dir.editorconfig}`, resolve, resolve);
     });
-
-
+  // _src install
   }).then(() => {
     title(`${cmd.install} [${dir.config}]`);
     execSync(`cd ${dir.config} && ${cmd.install}`, {stdio:[0,1,2]});
     title(``, true);
-
+  // build
   }).then(() => {
     if (isInitialize) {
       title(`${cmd.build}`);
       execSync(cmd.build, {stdio:[0,1,2]});
       title(``, true);
     }
-
   }).then(() => {
     console.log(colors.lightGreen(message.postInstallComplete));
-
   }).catch((e) => {
     console.log(e);
-
   });
-
-  function checkFile(fs, filepath) {
-    if (!fs) return;
-    try {
-      fs.statSync(filepath);
-      return true
-    } catch(err) {
-      if (err.code === 'ENOENT') {
-        return false;
-      }
-    }
-  }
 
   function title(message, isEnd = false, mark = '-', repeat = 30) {
     return console.log(isEnd
@@ -133,16 +86,20 @@
       ].join('\n'));
   }
 
-  function fsCopy(src, dest, opt, callback, resolve, reject) {
-    fs.copy(src, dest, opt, (err) => {
+  function copyFiles(src, dest, cbSuccess, cbFailure) {
+    fs.copy(src, dest, {
+      overwrite: false,
+      errorOnExist: true
+    }, (err) => {
       if (err) {
-        console.error(err);
-        callback && callback();
-        reject && reject();
+        // console.error(err);
+        console.log(message.notCopy)
+        title(``, true);
+        return cbFailure && cbFailure();
       }
       console.log(message.copyComplete);
-      callback && callback();
-      resolve && resolve();
+      title(``, true);
+      return cbSuccess && cbSuccess();
     });
   }
 })();
