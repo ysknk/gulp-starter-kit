03de71e85e0ebd52705e0f9fcc59b1552bc1ecdb
ysknk <31568245+ysknk@users.noreply.github.com>
2019-12-03 13:35:27 +0900

    Update mouseStalker.js

--- _src/htdocs/assets/js/_partials/shared_classes/mouseStalker.js
+++ _src/htdocs/assets/js/_partials/shared_classes/mouseStalker.js
@@ -30,6 +30,7 @@ export default ((win, doc) => {
         `position: fixed;`,
         `top: 0;`,
         `left: 0;`,
+        `z-index: 10000;`,
         `transform: translate(0, 0);`,
         `transition: all 0.2s ease-out;`,
         `pointer-events: none;`
