3100378458fed9205de8a174aaeb2fd6e9cb1d61
ysknk <31568245+ysknk@users.noreply.github.com>
2019-12-05 13:37:07 +0900

    Update mouseStalker.js

--- _src/htdocs/assets/js/_partials/shared_classes/mouseStalker.js
+++ _src/htdocs/assets/js/_partials/shared_classes/mouseStalker.js
@@ -25,6 +25,8 @@ export default ((win, doc) => {
       this.elem = '.js-mousestalker';
       this.activeClassName = 'is-active';
       this.throttleTime = 30;
+      this.throttleTimeScroll = 100;
+      this.throttleTimeResize = 100;
       this.debounceTime = this.throttleTime;
 
       this.initializeStyle = [
@@ -37,6 +39,9 @@ export default ((win, doc) => {
         `pointer-events: none;`
       ].join(``);
 
+      this.mouseX = 0;
+      this.mouseY = 0;
+
       _.isObject(opts_) && _.extend(this, opts_);
 
       // this.initialize();
@@ -48,28 +53,17 @@ export default ((win, doc) => {
     initialize() {
       this.setInitializeStyle();
 
-      const proc = (e) => {
-        if (!e.target || !e.target.closest) return;
-        let targetElem = this.targetElem ?
-          e.target.closest(this.targetElem) : doc;
-
-        let elem = doc.querySelector(this.elem);
-        elem.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
-
-        if (e.target === doc || !targetElem) {
-          this.onMouseLeave(e, elem);
-        } else {
-          this.onMouseEnter(e, elem);
-        }
-      };
-
       doc.addEventListener('mousemove', _.throttle((e) => {
-        proc(e);
+        this.procedure(e);
       }, this.throttleTime), false);
 
-      // doc.addEventListener('mouseup', _.throttle((e) => {
-      //   proc(e);
-      // }, this.throttleTime), false);
+      doc.addEventListener('scroll', _.throttle((e) => {
+        this.update();
+      }, this.throttleTimeScroll), false);
+
+      doc.addEventListener('resize', _.throttle((e) => {
+        this.update();
+      }, this.throttleTimeResize), false);
 
       // 画面外
       doc.addEventListener('mouseleave', _.debounce((e) => {
@@ -77,6 +71,62 @@ export default ((win, doc) => {
         let elem = doc.querySelector(this.elem);
         this.onMouseLeave(e, elem);
       }, this.debounceTime), false);
+    }
+
+    /**
+     * procedure
+     *
+     * @param {object} e event
+     */
+    procedure(e) {
+      if (!e.target) return;
+      let targetElem = this.targetElem ?
+        e.target.closest(this.targetElem) : doc;
+
+      let elem = doc.querySelector(this.elem);
+      elem.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
+      this.setMousePos(e.clientX, e.clientY);
+
+      if (e.target === doc || !targetElem) {
+        this.onMouseLeave(e, elem);
+      } else {
+        this.onMouseEnter(e, elem);
+      }
+    }
+
+    /**
+     * update
+     */
+    update() {
+      const mousePos = this.getMousePos();
+      if (!mousePos || !mousePos.x || !mousePos.y) return;
+      const elem = doc.elementFromPoint(mousePos.x, mousePos.y);
+      if (!elem) return;
+      this.procedure({
+        target: elem,
+        clientX: mousePos.x,
+        clientY: mousePos.y
+      });
+    }
+
+    /**
+     * getMousePos
+     *
+     * @returns {object} x, y
+     */
+    getMousePos() {
+      return this.mousePos || { x:0, y:0 };
+    }
+
+    /**
+     * setMousePos
+     *
+     * @param {number} x
+     * @param {number} y
+     * @returns {object} x, y
+     */
+    setMousePos(x, y) {
+      this.mousePos = {x, y};
     }
 
     /**
