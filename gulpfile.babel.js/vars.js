8b6b5664b021d3aa982d1ce1d3d7f997dd55b7b1
ysknk <31568245+ysknk@users.noreply.github.com>
2021-04-19 16:07:18 +0900

    Create vars.js

---
+++ gulpfile.babel.js/vars.js
@@ -0,0 +1,52 @@
+const define = {
+  config: 'config',
+  htdocs: 'htdocs'
+};
+
+const root = '../';
+
+const srcDir = `${root}_src/`;
+const destDir = `${root}${define.htdocs}/`;
+const tasksDir = 'tasks/';
+
+const src = (ext) => {
+  return [
+    `${srcDir}${define.htdocs}/**/*.${ext}`
+  ];
+};
+
+const ignore = (ext) => {
+  ext = ext ? `.${ext}` : '';
+  let src = [
+    `${define.htdocs}/**/_*`,
+    `${define.htdocs}/**/_**/**/*`
+  ].map((val) => {
+    return `${srcDir}${val}${ext}`;
+  });
+  src.push(`${srcDir}${define.config}/**/*`);
+  return src;
+};
+
+const path = {
+  srcDir,
+  destDir,
+  tasksDir,
+
+  config: `${srcDir}${define.config}/`,
+  htdocs: `${srcDir}${define.htdocs}/`,
+
+  pageConfig: `${srcDir}${define.config}/page.js`,
+  taskMaster:  './task/master',
+  taskConfigGlobal: `./task/${define.config}.js`,
+  taskConfigLocal: `${root}${srcDir}${define.config}/task.js`,
+  varsLocal: `${root}gulp.vars.js`,// merge with this file
+
+  src,
+  ignore,
+  dest: destDir
+}
+
+module.exports = {
+  ns: '__', // namespace
+  path
+};
