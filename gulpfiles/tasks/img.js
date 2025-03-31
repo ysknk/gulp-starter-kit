4e9ac7c5a44a8afd3ad2caeac8936468c30ae101
ysknk <31568245+ysknk@users.noreply.github.com>
2025-03-31 22:41:09 +0900

    Update img.js

--- gulpfiles/tasks/img.js
+++ gulpfiles/tasks/img.js
@@ -48,13 +48,12 @@ class Img extends TaskMaster {
         extname: this.task.data.extension
       })))
 
-      .pipe(gulp.dest(this.getDest()))
-      .on('finish', () => {done && done();})
-
       .pipe($.size(this.sizeOptions()))
       .pipe($.if(plugins.util.getIsWatch(), plugins.log()))
 
-      .pipe(this.serv());
+      .pipe(gulp.dest(this.getDest()))
+      .on('finish', () => {done && done();})
+      // .pipe(this.serv());
   }
 
   /**
